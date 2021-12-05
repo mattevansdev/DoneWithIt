9ed2806d90af4236de0bc02a6c6cf2d4acdfd43d
Matthew Evans <m@iammatthew.com>
2021-12-04 19:23:16 -0800

    List

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, TextInput, Text } from "react-native";
 import AppButton from "./app/components/AppButton";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
@@ -12,8 +12,26 @@ import Icon from "./app/components/Icon";
 import ListItem from "./app/components/ListItem";
 import AccountScreen from "./app/screens/AccountScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
+import AppTextInput from "./app/components/AppTextInput";
+import AppPicker from "./app/components/AppPicker";
+
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Cameras", value: 3 },
+]
 
 export default function App() {
-  return <ListingsScreen />;
+const [category, setCategory] = useState(categories[0]);
+
+  return (
+    <Screen>
+      <AppPicker 
+      selectedItem={category}
+      onSelectItem={item => setCategory(item)}
+      items={categories} icon="apps" placeholder ="Category" />
+      <AppTextInput icon="email" placeholder="Email"/>
+    </Screen>
+  );
 }
 
