1a2f7482db95ef393173ad61ed1d197fa114acbe
Matthew Evans <m@iammatthew.com>
2021-12-19 18:24:51 -0800

    Forms

--- App.js
+++ App.js
@@ -1,37 +1,11 @@
-import React, { useState } from "react";
-import { View, StyleSheet, TextInput, Text } from "react-native";
-import AppButton from "./app/components/AppButton";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-
-import Card from './app/components/Card'
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
-import AppTextInput from "./app/components/AppTextInput";
-import AppPicker from "./app/components/AppPicker";
-
-const categories = [
-  { label: "Furniture", value: 1 },
-  { label: "Clothing", value: 2 },
-  { label: "Cameras", value: 3 },
-]
+import React from "react";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
+import LoginScreen from "./app/screens/LoginScreen";
+import RegisterScreen from "./app/screens/RegisterScreen";
 
 export default function App() {
-const [category, setCategory] = useState(categories[0]);
-
   return (
-    <Screen>
-      <AppPicker 
-      selectedItem={category}
-      onSelectItem={item => setCategory(item)}
-      items={categories} icon="apps" placeholder ="Category" />
-      <AppTextInput icon="email" placeholder="Email"/>
-    </Screen>
+    <ListingEditScreen />
   );
 }
 
--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -1,25 +1,45 @@
-import React, { useState } from 'react'
-import { StyleSheet, Text, TextInput, TouchableWithoutFeedback, View, Modal, Button, FlatList } from 'react-native'
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import React, { useState } from "react";
+import {
+    View,
+    StyleSheet,
+    TouchableWithoutFeedback,
+    Modal,
+    Button,
+    FlatList,
+} from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-import defaultStyles from '../config/styles';
-import AppText from './AppText';
-import Screen from './Screen';
-import PickerItem from './PickerItem';
+import AppText from "./AppText";
+import Screen from "./Screen";
+import defaultStyles from "../config/styles";
+import PickerItem from "./PickerItem";
 
-export default function AppPicker({ icon, items, placeholder, onSelectItem, selectedItem }) {
+function AppPicker({ icon, items, onSelectItem, placeholder, selectedItem }) {
     const [modalVisible, setModalVisible] = useState(false);
+
     return (
         <>
             <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
                 <View style={styles.container}>
-                    {icon && <MaterialCommunityIcons name={icon}
+                    {icon && (
+                        <MaterialCommunityIcons
+                            name={icon}
+                            size={20}
+                            color={defaultStyles.colors.medium}
+                            style={styles.icon}
+                        />
+                    )}
+                    {selectedItem ? (
+                        <AppText style={styles.text}>{selectedItem.label}</AppText>
+                    ) : (
+                        <AppText style={styles.placeholder}>{placeholder}</AppText>
+                    )}
+
+                    <MaterialCommunityIcons
+                        name="chevron-down"
                         size={20}
-                        color={defaultStyles.colors.medium} style={styles.icon} />}
-                    <AppText style={styles.text}>{selectedItem ? selectedItem.label : placeholder}</AppText>
-                    <MaterialCommunityIcons name="chevron-down"
-                        size={20}
-                        color={defaultStyles.colors.medium} />
+                        color={defaultStyles.colors.medium}
+                    />
                 </View>
             </TouchableWithoutFeedback>
             <Modal visible={modalVisible} animationType="slide">
@@ -41,7 +61,7 @@ export default function AppPicker({ icon, items, placeholder, onSelectItem, sele
                 </Screen>
             </Modal>
         </>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
@@ -49,14 +69,20 @@ const styles = StyleSheet.create({
         backgroundColor: defaultStyles.colors.light,
         borderRadius: 25,
         flexDirection: "row",
-        width: '100%',
+        width: "100%",
         padding: 15,
-        marginVertical: 10
+        marginVertical: 10,
     },
     icon: {
-        marginRight: 10
+        marginRight: 10,
+    },
+    placeholder: {
+        color: defaultStyles.colors.medium,
+        flex: 1,
     },
     text: {
         flex: 1,
     },
-})
+});
+
+export default AppPicker;
